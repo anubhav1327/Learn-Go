bf500e42d02d29d58b26c62f8b652bb3b66e0a1e
anubhav1327194 <anubhavsingh1327@gmail.com>
2023-10-13 16:26:43 +0530

    functions type in go

--- 5.1Function/main.go
+++ 5.1Function/main.go
@@ -9,6 +9,8 @@ func main() {
 	b := b()
 	sum(a, b)
 	passbyref(&a, &b)
+	variadic(1, 2, 3, 4, 5, 56)
+	// initfun()
 }
 
 func a() int {
@@ -24,4 +26,24 @@ func sum(a, b int) {
 
 func passbyref(a, b *int) {
 	fmt.Println("Pass by reference = ", *a+*b)
+}
+
+// Functions type
+
+// 1- Pass by value
+// 2- Pass by reference
+// 3- Anonymous Function
+// 4- Variadic function
+// 5- init Function
+
+// variadic function -chahe jitni value di jaye sab accept kar lega or sabse pahle run hoga mtlb jab ye run ho jayega tabhi baki code run hoga to ham ispe api fetch krke jab api aa jaye ya jab sab ready ho jaye tab baki ka code run kr dakte hai ise use krke
+
+func variadic(v ...int) {
+	fmt.Println("element are", v)
+}
+
+// init function
+
+func init() {
+	fmt.Println("This is init function - na koi value input lega na hi koi value return dega")
 }
